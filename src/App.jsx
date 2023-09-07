12b4eaf3dd8c4fd45008f5e4241c6ed08a7e6c81
sawyerno <sawyernoland2005@gmail.com>
2023-09-06 21:26:58 -0400

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
-import { useState } from "react";
-const Color = (props) => {
-  return <div className={props.color}></div>
+import { useState } from 'react'
+const [selectedColor, setSelectedColor] = useState("")
+const Color = ({color, setSelectedColor}) => {
+  return (
+    <div 
+      className={color} 
+      onClick={() => setSelectedColor(color)} 
+     ></div>
+  )
 }
 const App = () => {
   return (
     <div id="container">
       <div id="navbar">
-        <div>Currently selected: </div>
-        <div className="red">red</div>
-      </div>
+       <div>Currently selected: </div>
+       <div className={selectedColor}>{selectedColor}</div>
+    </div>
       <div id="colors-list">
-        <Color color="green"/>
-        <Color color="blue"/>
-        <Color color="violet"/>
+        <Color color="red" setSelectedColor={setSelectedColor}/>
+        <Color color="blue" setSelectedColor={setSelectedColor}/>
+        <Color color="green" setSelectedColor={setSelectedColor}/>
       </div>
     </div>
   );
